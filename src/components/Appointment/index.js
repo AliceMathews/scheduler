5e7371905b39b39eed40b46cb8e6f540dfd47591
AliceMathews <alice_mathews@hotmail.co.uk>
2020-01-22 14:19:47 -0800

    added create mode and conditional to render the form when mode state is create

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,10 +6,12 @@ import useVisualMode from "hooks/useVisualMode";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import Form from "components/Appointment/Form";
 
 export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
+  const CREATE = "CREATE";
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
@@ -19,11 +21,26 @@ export default function Appointment(props) {
     <article className="appointment">
       <Header time={props.time} />
 
-      {mode === EMPTY && <Empty onAdd={props.onAdd} />}
+      {mode === EMPTY && (
+        <Empty
+          onAdd={() => {
+            transition(CREATE);
+          }}
+        />
+      )}
       {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          // onSave={}
+          onCancel={() => {
+            back();
+          }}
         />
       )}
     </article>

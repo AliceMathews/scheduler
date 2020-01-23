7f90473aeef17595d83138eff954cda7c9f47f8a
AliceMathews <alice_mathews@hotmail.co.uk>
2020-01-22 16:42:01 -0800

    application now transitions to saving whilst the axios put request completes and renders the status component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,11 +7,13 @@ import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
 import Form from "components/Appointment/Form";
+import Status from "components/Appointment/Status";
 
 export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
+  const SAVING = "SAVING";
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
@@ -23,6 +25,7 @@ export default function Appointment(props) {
       interviewer
     };
 
+    transition(SAVING);
     props.bookInterview(props.id, interview).then(() => transition(SHOW));
   }
 
@@ -54,6 +57,7 @@ export default function Appointment(props) {
           }}
         />
       )}
+      {mode === SAVING && <Status message={mode} />}
     </article>
   );
 }
